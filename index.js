83e79c5a19d56a6fd0a9eef09e8de609debcd52f
Hriday Keswani <hridaykeswani@gmail.com>
2021-11-06 17:14:11 +0530

    made changes to routiing and added images and styles folder

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use("/images",express.static("images"));
+app.use("/styles",express.static("styles"));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('./home/home');
     res.end();
 });
 
