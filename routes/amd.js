1ac225d5bcb2237e4f3e95c528050cad59d24ce4
Hriday Keswani <hridaykeswani@gmail.com>
2021-11-08 10:44:17 +0530

    added routing for amd cpus

--- routes/amd.js
+++ routes/amd.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
-router.get('/motherboards', (req, res) => {
-    res.send('motherboards');
+let CPU;
+
+fs.readFile('build_data/cpu.json', (err,data) => {
+    if (err) 
+        throw err;
+    else
+    CPU = JSON.parse(data);
+});
+
+router.get('/cpu', (req, res) => {
+    res.render('amd/cpu',{cpus:CPU});
+});
+
+router.get('/cpu/:cpuName/motherboards', (req, res) => {
+    res.send(req.params.cpuName);
 });
 
 module.exports = router;
