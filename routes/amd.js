bba560acd4ddb8bed814a62137d5335c0acfc873
Hriday Keswani <hridaykeswani@gmail.com>
2021-11-15 10:32:55 +0530

    made routes for AMD up till coolers

--- routes/amd.js
+++ routes/amd.js
@@ -3,12 +3,44 @@ const router = express.Router();
 const fs = require('fs');
 
 let CPU;
+let motherboard;
+let GPU;
+let coolers;
+let storage;
+
+fs.readFile('build_data/motherboard.json', (err, data) => {
+    if (err) 
+        throw err;
+    else
+        motherboard = JSON.parse(data);
+});
 
 fs.readFile('build_data/cpu.json', (err,data) => {
     if (err) 
         throw err;
     else
-    CPU = JSON.parse(data);
+        CPU = JSON.parse(data);
+});
+
+fs.readFile('build_data/vc.json', (err,data) => {
+    if (err) 
+        throw err;
+    else
+        GPU = JSON.parse(data);
+});
+
+fs.readFile('build_data/cpu-cooler.json', (err,data) => {
+    if (err) 
+        throw err;
+    else
+        coolers = JSON.parse(data);
+});
+
+fs.readFile('build_data/internal-hard-drive.json', (err,data) => {
+    if (err) 
+        throw err;
+    else
+        storage = JSON.parse(data);
 });
 
 router.get('/cpu', (req, res) => {
@@ -16,7 +48,20 @@ router.get('/cpu', (req, res) => {
 });
 
 router.get('/cpu/:cpuName/motherboards', (req, res) => {
-    res.send(req.params.cpuName);
+    let selcpu = req.params.cpuName;
+    res.render('amd/motherboard',{ selcpu ,mbds:motherboard});
+});
+
+router.get('/cpu/:cpuName/motherboards/:motherboardName/gpu', (req, res) => {
+    res.render('amd/gpu',{gpu:GPU});
+});
+
+router.get('/cpu/:cpuName/motherboards/:motherboardName/gpu/:gpuName/coolers', (req, res) => {
+    res.render('amd/coolers',{coolers:coolers});
+});
+
+router.get('/cpu/:cpuName/motherboards/:motherboardName/gpu/:gpuName/coolers/:coolerName/storage', (req, res) => {
+    res.render('amd/storage',{storage:storage});
 });
 
 module.exports = router;
