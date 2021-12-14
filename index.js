e5421d8d649f6e052dbec9be4f35b97c547b0509
Hriday Keswani <hridaykeswani@gmail.com>
2021-12-14 18:04:40 +0530

    set up prebuild backend

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 app.use("/images",express.static("images"));
 app.use("/styles",express.static("styles"));
+
+let prebuild;
+
+fs.readFile('./build_data/pb.json', function (err, data) {
+    if (err) 
+        throw err;
+    prebuild = JSON.parse(data);
+    console.log(prebuild);
+});
 
 app.get('/', (req, res) => {
     res.render('./home');
@@ -17,6 +27,11 @@ app.get('/build', (req,res) => {
 
 app.get('/doc',(req,res) => {
     res.render('./documentation/doc');
+    res.end();
+});
+
+app.get('/prebuild',(req,res) => {
+    res.render('./options',{ pb:prebuild });
     res.end();
 });
 
